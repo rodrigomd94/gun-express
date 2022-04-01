2c6d1e6572aa44dabaeb4d77bc39a5caff8a8ed1
rodrigomd94 <rodrigomd94@gmail.com>
2022-03-31 22:31:58 -0600

    fixed dependencies

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const GUN = require('gun/gun');
-const server = require('http').createServer().listen(8080);
-const gun = GUN({web: server});
+console.log("If module not found, install express globally `npm i express -g`!");
+var port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8080;
+var express = require('express');
+var Gun     = require('gun');
+require('axe');
+
+var app    = express();
+app.use(Gun.serve);
+app.use(express.static(__dirname));
+
+var server = app.listen(port);
+var gun = Gun({	file: 'data', web: server });
+
+global.Gun = Gun; /// make global to `node --inspect` - debug only
+global.gun = gun; /// make global to `node --inspect` - debug only
+
+console.log('Server started on port ' + port + ' with /gun');
